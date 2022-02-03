839e922d010af307bdce34bc3b12415ba9a39e35
jackbaihaochen <jackbaihaochen@gmail.com>
2022-02-03 21:58:14 +0900

    testing

--- lineworks.py
+++ lineworks.py
@@ -286,7 +286,7 @@ class LineBot():
         }
         data = json.dumps(data)
         response = requests.post(url=url, data=data, headers=headers)
-        print('Message sent. Response: ' + json.dumps(response.json()))
+        print('Message sent. Response: ' + response.text)
         response = response.status_code
         print('Message sent. Response: \n' + 'Succeeded' if response == 201 else 'Failed')
         return response == 201
